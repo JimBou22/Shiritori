3e10bbee81efecf348a3642be749a7bb4cc00cc8
Jimmy <Jimmy@192.168.1.145>
2020-03-18 20:40:45 -0700

    Finished base code

--- src/omg/Shiritori.java
+++ src/omg/Shiritori.java
@@ -5,69 +5,69 @@ import java.io.*;
 
 public class Shiritori {
 	static boolean win = false;
-	static boolean lose = false;
 	static File file = new File("src/lib/american-words.95");
 	static List<String> list = new ArrayList<String>();
 	static List<String> used = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		// import/build dictionary of words
 
-		Scanner sc = new Scanner(System.in);
 		try {
+			// import/build dictionary of words
 			buildDictionary();
-
-			// get user input from console with menu face
-			System.out.println("Welcome to the Word Chain Game!");
-			System.out.println("You know the rules :)");
-			System.out.println("Please enter a word");
-
-			String input = sc.nextLine();
-
-			// check if word
-			// a. Exists as a word previously used
-			// b. Has an answer retort in the dictionary provided.
-
-			if (!used.contains(input)) {
-				used.add(input);
-				Collections.shuffle(list);
-
-				String prev = "";
-				for (int i = 0; i < list.size(); i++) {
-					char last = input.charAt(input.length() - 1);
-					if (list.get(i).charAt(0) == last) {
-						System.out.println(list.get(i));
-						prev = list.get(i);
-						list.remove(i);
-						break;
-					}
-				}
-				System.out.println(list.size());
-				if (list.size() < 1 || list == null) {
-					win = true;
-				}
-
-				// replay with having to start with last letter of previous word
-
-				playAgain(prev);
-
-				// win case/lose case
-				if (win == true) {
-					System.out.println("You cheated");
-					sc.close();
-					System.exit(0);
-				}
-			} else {
-				lose = true;
-				System.out.println("You suck");
-			}
-
+			playFirst();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 
 		}
 
+	}
+
+	private static void playFirst() {
+		// get user input from console with menu face
+		System.out.println("Welcome to the Word Chain Game!");
+		System.out.println("You know the rules :)");
+		System.out.println("Please enter a word");
+
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+
+		// check if word
+		// a. Exists as a word previously used
+		// b. Has an answer retort in the dictionary provided.
+
+		if (!used.contains(input)) {
+			used.add(input);
+			Collections.shuffle(list);
+
+			String prev = "";
+			for (int i = 0; i < list.size(); i++) {
+				char last = input.charAt(input.length() - 1);
+				if (list.get(i).charAt(0) == last) {
+					System.out.println(list.get(i));
+					prev = list.get(i);
+					list.remove(i);
+					break;
+				}
+			}
+			getListSize(list);
+			if (list.size() < 1 || list == null) {
+				win = true;
+			}
+
+			// replay with having to start with last letter of previous word
+
+			playAgain(prev);
+
+			// win case/lose case
+			if (win == true) {
+				System.out.println("You cheated");
+				sc.close();
+				System.exit(0);
+			}
+		} else {
+			lose();
+		}
 	}
 
 	private static void buildDictionary() throws FileNotFoundException, IOException {
@@ -81,18 +81,66 @@ public class Shiritori {
 	}
 
 	private static void playAgain(String prev) {
-		System.out.println("Youre previous word was " + prev);
-		System.out.println("Provide a word that starts with " + prev.charAt(prev.length() - 1));
+		System.out.println("Your previous word was " + prev);
+		System.out.println("Provide a word that starts with " + lastChar(prev));
 		play(prev);
 	}
 
 	private static void play(String prev) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter another word");
+		String input = sc.nextLine();
 
+		// check if word
+		// a. Exists as a word previously used
+		// b. Has an answer retort in the dictionary provided.
+		if(input.charAt(0) != lastChar(prev)) {
+			lose();
+		}
+		
+		if (!used.contains(input)) {
+			used.add(input);
+			Collections.shuffle(list);
+
+			String prev1 = "";
+			for (int i = 0; i < list.size(); i++) {
+				char last = input.charAt(input.length() - 1);
+				if (list.get(i).charAt(0) == last) {
+					System.out.println(list.get(i));
+					prev1 = list.get(i);
+					list.remove(i);
+					break;
+				}
+			}
+			getListSize(list);
+			if (list.size() < 1 || list == null) {
+				win = true;
+			} else {
+				playAgain(prev1);
+			}
+
+			// win case/lose case
+			if (win == true) {
+				System.out.println("You cheated");
+				sc.close();
+				System.exit(0);
+			}
+		} else {
+			lose();
+		}
+	}
+
+	private static void lose() {
+		System.out.println("You suck");
+		System.exit(0);
 	}
 
 	private static void getListSize(List<String> list) {
-		System.out.println(list.size());
+		System.out.println(list.size() + " words left in the bank");
+	}
+	
+	private static char lastChar(String prev) {
+		return prev.charAt(prev.length() - 1);
 	}
 
 }
